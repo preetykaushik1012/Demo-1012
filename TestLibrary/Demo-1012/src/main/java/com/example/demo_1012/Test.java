966e5274f4a92ddbb3f529d4c211fb4917d8df0c
desktop-42 <preeti.kaushik@9spl.com>
2022-06-07 14:26:32 +0530

    initial commit

---
+++ TestLibrary/Demo-1012/src/main/java/com/example/demo_1012/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo_1012;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    public static void s(Context c, String message){
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ TestLibrary/PaymentGateway/src/main/java/com/example/paymentgateway/Test.java
@@ -0,0 +1,10 @@
+package com.example.paymentgateway;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+    public static void s(Context c, String message){
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+    }
+}
