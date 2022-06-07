6fbb6e56c09dc4177a078f3f21efa58e33364943
desktop-42 <preeti.kaushik@9spl.com>
2022-06-07 14:46:37 +0530

    initial commit

---
+++ Demo-1012/src/main/java/com/example/MessageTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MessageTest {
+    public static void s(Context c, String message){
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+    }
+}
