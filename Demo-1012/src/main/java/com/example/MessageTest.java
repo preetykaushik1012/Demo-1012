192e464abb182c4e6dc3b40f8ae6f321e194c179
desktop-42 <preeti.kaushik@9spl.com>
2022-06-08 10:35:26 +0530

    Changes in toast

--- Demo-1012/src/main/java/com/example/MessageTest.java
+++ Demo-1012/src/main/java/com/example/MessageTest.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class MessageTest {
-    public static void s(Context c, String message){
+//    public static void s(Context c, String message){
+//        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+//    }
+    public static void testToast(Context c, String message){
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
     }
 }
